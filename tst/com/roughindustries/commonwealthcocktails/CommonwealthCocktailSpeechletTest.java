449bdd45f864af9b52eb1e81a2309d36e295619e
roughindustries <travishdc@roughindustries.com>
2016-06-06 14:31:43 -0500

    added more to the phrases to handle more db info

--- tst/com/roughindustries/commonwealthcocktails/CommonwealthCocktailSpeechletTest.java
+++ tst/com/roughindustries/commonwealthcocktails/CommonwealthCocktailSpeechletTest.java
@@ -2,14 +2,19 @@ package com.roughindustries.commonwealthcocktails;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
+import org.stringtemplate.v4.ST;
+import org.stringtemplate.v4.STGroup;
+import org.stringtemplate.v4.STGroupFile;
 
 import com.roughindustries.commonwealthcocktails.model.ccCocktail;
 
@@ -21,24 +26,44 @@ public class CommonwealthCocktailSpeechletTest {
 	public void testGetCocktailRecipeResponse() {
 		InputStream inputStream;
 		long lStartTime = System.currentTimeMillis();
-
+		SqlSession dbsession = null;
+		List<ccCocktail> cocktailList = null;
 		try {
 			inputStream = Resources.getResourceAsStream(mybatisConfigFileName);
 
 			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-			SqlSession dbsession = sqlSessionFactory.openSession();
+			dbsession = sqlSessionFactory.openSession();
 			// cu.getCocktails(dbsession, "After 8");
 			// cu.getCocktails(dbsession, "After Eight");
 			// cu.getCocktails(dbsession, "Whiskey Sour");
-			ccCocktail.getCocktails(dbsession, "margarita on the rocks");
+			//cocktailList = ccCocktail.getCocktails(dbsession, "margarita on the rocks");
 			// cu.getCocktails(dbsession, "margarita");
 			// cu.getCocktails(dbsession, "Eighteen 97");
 			// cu.getCocktails(dbsession, "1897");
-			// cu.getCocktails(dbsession, "Ricaço");
+			cocktailList = ccCocktail.getCocktails(dbsession, "Ricaço");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally {
+			if (dbsession != null) {
+				dbsession.close();
+				dbsession = null;
+			}
 		}
+		ClassLoader classLoader = this.getClass().getClassLoader();
+		File file = new File(classLoader.getResource("speechAssets/reponsePhrases.stg").getFile());
+
+		STGroup group = null;
+		try {
+			group = new STGroupFile(file.getCanonicalPath(), '$', '$');
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		ST st = group.getInstanceOf("echoSpeechRecipe");
+		st.add("cocktail", cocktailList.get(0));
+		String speechOutput = st.render();
+		System.out.println(speechOutput);
 		long lEndTime = System.currentTimeMillis();
 		long difference = lEndTime - lStartTime;
 		System.out.println("Elapsed seconds: " + difference / 1000);
